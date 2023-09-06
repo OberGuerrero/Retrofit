4caac673992a5f7a951029973134eda47941c2f2
OberGuerrero <eguerrero@uniempresarial.edu.co>
2023-09-06 17:54:07 -0500

    Retrofit 06092023

--- app/src/main/java/co/edu/ue/retrofit/MainActivity.java
+++ app/src/main/java/co/edu/ue/retrofit/MainActivity.java
@@ -1,10 +1,48 @@
 package co.edu.ue.retrofit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    PrincipalFragment homeFragment = new PrincipalFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+
+    private void loadFragment(Fragment fr){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fr);
+        transaction.commit();
+    }
+
+    private void selectionFr(BottomNavigationView navigation){
+        navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.first:
+                        loadFragment(homeFragment);
+                        return true;
+                    case R.id.second:
+                        loadFragment(secondFragment);
+                        return true;
+                    case R.id.third:
+                        loadFragment(thirdFragment);
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/co/edu/ue/retrofit/SecondFragment.java
@@ -0,0 +1,32 @@
+package co.edu.ue.retrofit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SecondFragment extends Fragment {
+
+    public SecondFragment() {
+    }
+
+    public static SecondFragment newInstance(String param1, String param2) {
+        SecondFragment fragment = new SecondFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+}
