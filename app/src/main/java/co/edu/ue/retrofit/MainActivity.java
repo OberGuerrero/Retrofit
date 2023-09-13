5bc104e87c3e8e432a3ffed4c38a98889b216eb1
OberGuerrero <eguerrero@uniempresarial.edu.co>
2023-09-13 17:46:25 -0500

    ultimo cambio 1309

--- app/src/main/java/co/edu/ue/retrofit/MainActivity.java
+++ app/src/main/java/co/edu/ue/retrofit/MainActivity.java
@@ -48,5 +48,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        selectionFr(navigationView);
+        loadFragment(homeFragment);
     }
 }
